be4c9cfe00daa72ca8a3528dab6d5998ad8116c6
Bjose <bjose2100@gmail.com>
2025-06-05 00:59:15 -0500

    Hilos y Algoritmos hechos

--- threads_sockets/client_side.py
+++ threads_sockets/client_side.py
@@ -1,5 +1,5 @@
 import socket
-SERVER = "192.168.1.8"
+SERVER = "192.168.128.21"
 PORT = 8080
 client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 client.connect((SERVER, PORT))
@@ -11,4 +11,4 @@ while True:
     client.sendall(bytes(out_data, 'UTF-8'))
     if out_data == 'bye':
         break
-client.close()
+client.close()  
